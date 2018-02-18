5678cb93bcb06e2d83fabecc38048ba467bce49f
Madhu <madhu1polaki@gmail.com>
2018-02-18 14:55:44 +0530

    Second version of HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ package com.madhu.git;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hi Madhu");
+		System.out.println("Hi Madhu Good Morning");
 
 	}
 
